6e1204d85702ba6d33c27326366ae3f83eef2969
Honking <515204795@qq.com>
2019-08-09 16:28:07 +0800

    2019.8.9 第一次全部提交

---
+++ springbootmvcwebbatis/src/main/java/com/neusoft/bumis/SpringbootmvcwebbatisApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.bumis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcwebbatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcwebbatisApplication.class, args);
+	}
+
+}
